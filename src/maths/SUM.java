4295436651a7feadc22a703101358d2daf1a6732
Zhiyong Zhao <zhiyong.zhao@duke.edu>
2017-02-26 13:11:34 -0500

    some maths

---
+++ src/maths/SUM.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package maths;
+
+/**
+ * @author Zhiyong
+ *
+ */
+public class SUM extends Maths{
+	public SUM(){
+		
+	}
+
+	@Override
+	public double executeCommand() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
