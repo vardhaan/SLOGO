75406ff5d5dd366f2be3c16b48062789e514458f
Zhiyong Zhao <zhiyong.zhao@duke.edu>
2017-03-07 21:41:21 -0500

    setbackground

--- src/commands/SETBACKGROUND.java
+++ src/commands/SETBACKGROUND.java
@@ -3,6 +3,8 @@
  */
 package commands;
 
+import java.util.ResourceBundle;
+
 import gui.GridBuilder;
 
 /**
@@ -10,18 +12,24 @@ import gui.GridBuilder;
  *
  */
 public class SETBACKGROUND extends Command{
+	
+	public static final String RESOURCE_BUNDLE = "index/backgroundcolor";
+	
 	public SETBACKGROUND(){
 		super();
 		expectedNumParameters = 1;
+		
 	}
 
 	@Override
 	public double executeCommand() {
+		ResourceBundle resources = ResourceBundle.getBundle(RESOURCE_BUNDLE);
 		GridBuilder grid = new GridBuilder();
 		//TODO:add a method in the grid to set the background
 		returnValue = parameters.get(0);
-		grid.setBackgroundColor(returnValue);
-		return 0;
+		//set the color of the grid to be the color at index = returnValue
+		grid.setBackgroundColor(resources.getString(returnValue+""));
+		return returnValue;
 	}
 
 }
